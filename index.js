6927fb84b1fc96f6ac51b97157720e36cb69745b
Bryce Hanscomb <bryce@brycehanscomb.com>
2020-01-08 22:10:12 +1100

    add fadeout on click start button

--- index.js
+++ index.js
@@ -7,19 +7,40 @@ customElements.define("circles-viz", CirclesViz);
 const minutesPicker = document.querySelector("quantity-picker");
 const circlesViz = document.querySelector("circles-viz");
 const minutesLabel = document.querySelector(".minutes-label");
+const startButton = document.querySelector(".start-button");
 
 const MIN_MINUTES = 1;
 const MAX_MINUTES = 5;
 
+let state = {
+  currentStep: 1,
+  minutes: 1
+};
+
+startButton.addEventListener("click", () => {
+  state = {
+    ...state,
+    currentStep: state.currentStep + 1
+  };
+
+  render(state);
+});
+
 minutesPicker.addEventListener("change", e => {
   const { value } = e.detail;
+
   if (value <= MAX_MINUTES && value >= MIN_MINUTES) {
-    render({ minutes: value });
+    state = {
+      ...state,
+      minutes: value
+    };
+
+    render(state);
   }
 });
 
 /**
- * @param {{ minutes: number }} state
+ * @param {{ minutes: number, currentStep: number }} state
  */
 const render = state => {
   minutesPicker.setAttribute("value", state.minutes.toString());
@@ -31,4 +52,21 @@ const render = state => {
   } else {
     minutesLabel.classList.remove("hidden");
   }
+
+  const step1Container = document.getElementById("step-1");
+
+  if (state.currentStep === 1) {
+    step1Container.style.display = "flex";
+    step1Container.classList.remove("inactive");
+    step1Container.classList.add("active");
+  } else {
+    step1Container.classList.remove("active");
+    step1Container.classList.add("inactive");
+
+    setTimeout(() => {
+      step1Container.style.display = "none";
+    }, 500);
+  }
 };
+
+render(state);
